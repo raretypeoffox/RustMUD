9b5357d869f0857bae6e7db6b63a3479f9bc4514
raretypeoffox <raretypeoffox@gmail.com>
2024-01-28 22:43:45 -0800

    added mob parsing, managing templates

--- src/world.rs
+++ src/world.rs
@@ -22,7 +22,7 @@ enum State {
 struct Game {
     area_manager: AreaManager,
     room_manager: RoomManager,
-    // mob_manager: MobManager,
+    mob_manager: MobManager,
     // obj_manager: ObjManager,
     // reset_manager: ResetManager,
     // shop_manager: ShopManager,
@@ -30,14 +30,14 @@ struct Game {
 }
 
 pub struct Area {
-    min_vnum: i32,
-    max_vnum: i32,
+    min_vnum: u16,
+    max_vnum: u16,
     builder: String,
     name: String,
 }
 
 impl Area {
-    pub fn new(min_vnum: i32, max_vnum: i32, builder: String, name: String) -> Self {
+    pub fn new(min_vnum: u16, max_vnum: u16, builder: String, name: String) -> Self {
         Self { min_vnum, max_vnum, builder, name }
     }
 }
@@ -58,13 +58,68 @@ impl AreaManager {
     // Other area management methods...
 }
 
+pub struct MobTemplate {
+    vnum: u16,
+    keywords: String,
+    short_desc: String,
+    long_desc: String,
+    desc: String,
+    act_flags: u32,
+    aff_flags: u32,
+    align: i16,
+    level: u16,
+    hitroll: i16,
+    ac: i16,
+    hitdice_num: i16,
+    hitdice_size: i16,
+    hitdice_bonus: i16,
+    damdice_num: i16,
+    damdice_size: i16,
+    damdice_bonus: i16,
+    gold: i32,
+    xp: i32,
+    sex: String,
+    speed: i8,
+}
+
+impl MobTemplate {
+    pub fn new(vnum: u16, keywords: String, short_desc: String, long_desc: String, desc: String, act_flags: u32, aff_flags: u32, align: i16, level: u16, hitroll: i16, ac: i16, hitdice_num: i16, hitdice_size: i16, hitdice_bonus: i16, damdice_num: i16, damdice_size: i16, damdice_bonus: i16, gold: i32, xp: i32, sex: String, speed: i8) -> Self {
+        Self { vnum, keywords, short_desc, long_desc, desc, act_flags, aff_flags, align, level, hitroll, ac, hitdice_num, hitdice_size, hitdice_bonus, damdice_num, damdice_size, damdice_bonus, gold, xp, sex, speed }
+    }
+}
+
+pub struct MobManager {
+    mobs: HashMap<u16, MobTemplate>,
+}
+
+
+impl MobManager {
+    fn new() -> Self {
+        Self {
+            mobs: HashMap::new(),
+        }
+    }
+
+    fn add(&mut self, id: u16, mob: MobTemplate) {
+        self.mobs.insert(id, mob);
+    }
+
+    fn get(&self, id: u16) -> Option<&MobTemplate> {
+        self.mobs.get(&id)
+    }
+
+    fn get_all(&self) -> Vec<&MobTemplate> {
+        self.mobs.values().collect()
+    }
+}
+
 struct Room {
-    vnum: i32,
+    vnum: u16,
     name: String,
     description: String,
-    area_number: i32,
-    room_flags: i32,
-    sector_type: i32,
+    area_number: u16,
+    room_flags: u32,
+    sector_type: u32,
     doors: HashMap<i32, Door>,
     extended_descriptions: Vec<ExtendedDescription>,
     mob_list: HashSet<i32>,
@@ -75,7 +130,7 @@ struct Room {
 }
 
 impl Room {
-    fn new(vnum: i32) -> Self {
+    fn new(vnum: u16) -> Self {
         Self {
             vnum,
             name: String::new(),
@@ -104,16 +159,16 @@ impl Room {
 
 #[derive(Eq, PartialEq, Hash)]
 struct Door {
-    door_number: i32,
+    door_number: u8,
     description: String,
     keywords: String,
-    locks: i32,
-    key: i32,
-    to_room: i32,
+    locks: u16,
+    key: u16,
+    to_room: u16,
 }
 
 impl Door {
-    fn new(door_number: i32, description: String, keywords: String, locks: i32, key: i32, to_room: i32) -> Self {
+    fn new(door_number: u8, description: String, keywords: String, locks: u16, key: u16, to_room: u16) -> Self {
         Self {
             door_number,
             description,
@@ -157,26 +212,26 @@ impl ExtendedDescription {
 }
 
 struct RoomManager {
-    items: HashMap<i32, Room>,
+    rooms: HashMap<u16, Room>,
 }
 
 impl RoomManager {
     fn new() -> Self {
         Self {
-            items: HashMap::new(),
+            rooms: HashMap::new(),
         }
     }
 
-    fn add(&mut self, id: i32, item: Room) {
-        self.items.insert(id, item);
+    fn add(&mut self, id: u16, room: Room) {
+        self.rooms.insert(id, room);
     }
 
-    fn get(&self, id: i32) -> Option<&Room> {
-        self.items.get(&id)
+    fn get(&self, id: u16) -> Option<&Room> {
+        self.rooms.get(&id)
     }
 
     fn get_all(&self) -> Vec<&Room> {
-        self.items.values().collect()
+        self.rooms.values().collect()
     }
 }
 
@@ -199,9 +254,9 @@ fn parse_multi_line(lines: &[String]) -> Result<(String, usize), &'static str> {
 }
 
 
-fn parse_flags(flag_string: &str) -> Result<i32, std::num::ParseIntError> {
+fn parse_flags(flag_string: &str) -> Result<u32, std::num::ParseIntError> {
     if flag_string.contains('|') {
-        let numbers: Result<Vec<i32>, _> = flag_string.split('|').map(str::parse).collect();
+        let numbers: Result<Vec<u32>, _> = flag_string.split('|').map(str::parse).collect();
         numbers.map(|nums| nums.iter().sum())
     } else {
         flag_string.parse()
@@ -211,8 +266,8 @@ fn parse_flags(flag_string: &str) -> Result<i32, std::num::ParseIntError> {
 fn parse_area(game: &mut Game, line: &str) -> io::Result<()> {
     let re = Regex::new(r"#AREA\s*\{\s*?(\d+)\s+(\d+)\}\s*(\w+)\s+(.*)~").unwrap();
     if let Some(caps) = re.captures(line) {
-        let min_vnum = caps[1].parse::<i32>().expect("Invalid min vnum");
-        let max_vnum = caps[2].parse::<i32>().expect("Invalid max vnum");
+        let min_vnum = caps[1].parse::<u16>().expect("Invalid min vnum");
+        let max_vnum = caps[2].parse::<u16>().expect("Invalid max vnum");
         let builder = &caps[3];
         let name = &caps[4];
 
@@ -226,18 +281,86 @@ fn parse_area(game: &mut Game, line: &str) -> io::Result<()> {
     Ok(())
 }
 
-//     room_manager.add(vnum, current_room)
+fn parse_mob(game: &mut Game, lines: &mut Vec<String>) {
+
+    let mob_vnum: u16 = lines.remove(0)[1..].parse().expect("Error parsing mob vnum");
+
+    let (mob_keywords, mut offset) = match parse_multi_line(&lines[1..]) {
+        Ok(result) => result,
+        Err(_) => {
+            eprintln!("Error parsing multi line");
+            return;
+        }
+    };
+    let mob_keywords = mob_keywords.to_lowercase();
+    
+    let (mob_short_desc, offset_add) = match parse_multi_line(&lines[offset..]) {
+        Ok(result) => result,
+        Err(_) => {
+            eprintln!("Error parsing multi line");
+            return;
+        }
+    };
+    offset += offset_add;
+    
+    let (mob_long_desc, offset_add) = match parse_multi_line(&lines[offset..]) {
+        Ok(result) => result,
+        Err(_) => {
+            eprintln!("Error parsing multi line");
+            return;
+        }
+    };
+    offset += offset_add;
+    
+    let (mob_desc, offset_add) = match parse_multi_line(&lines[offset..]) {
+        Ok(result) => result,
+        Err(_) => {
+            eprintln!("Error parsing multi line");
+            return;
+        }
+    };
+    offset += offset_add;
+    
+    let parts: Vec<&str> = lines[offset].split_whitespace().collect();
+    let act_flags = parse_flags(parts[0]).expect("Error parsing act flags");
+    let aff_flags = parse_flags(parts[1]).expect("Error parsing aff flags");
+    let align: i16 = parts[2].parse().expect("Error parsing align");
+    let offset = offset + 1;
+    
+    let parts: Vec<&str> = lines[offset].split_whitespace().collect();
+    let level: u16 = parts[0].parse().expect("Error parsing level");
+    let hitroll: i16 = parts[1].parse().expect("Error parsing hitroll");
+    let ac: i16 = parts[2].parse().expect("Error parsing ac");
+    let replaced = parts[3].replace("D", "d");
+    let dice: Vec<&str> = replaced.split('d').collect();
+    let hitdice_num: i16 = dice[0].parse().expect("Error parsing hitdice num");
+    let dice: Vec<&str> = dice[1].split('+').collect();
+    let hitdice_size: i16 = dice[0].parse().expect("Error parsing hitdice size");
+    let hitdice_bonus: i16 = dice[1].parse().expect("Error parsing hitdice bonus");
+    let replaced = parts[4].replace("D", "d");
+    let dice: Vec<&str> = replaced.split('d').collect();
+    let damdice_num: i16 = dice[0].parse().expect("Error parsing damdice num");
+    let dice: Vec<&str> = dice[1].split('+').collect();
+    let damdice_size: i16 = dice[0].parse().expect("Error parsing damdice size");
+    let damdice_bonus: i16 = dice[1].parse().expect("Error parsing damdice bonus");
+    let offset = offset + 1;
+    
+    let parts: Vec<&str> = lines[offset].split_whitespace().collect();
+    let gold: i32 = parts[0].parse().expect("Error parsing gold");
+    let xp: i32 = parts[1].parse().expect("Error parsing xp");
+    let offset = offset + 1;
+    
+    let parts: Vec<&str> = lines[offset].split_whitespace().collect();
+    let sex: String = parts[2].to_string();
+    
+    let current_mob = MobTemplate::new(mob_vnum, mob_keywords, mob_short_desc, mob_long_desc, mob_desc, act_flags, aff_flags, align, level, hitroll, ac, hitdice_num, hitdice_size, hitdice_bonus, damdice_num, damdice_size, damdice_bonus, gold, xp, sex, 3);
+    game.mob_manager.add(mob_vnum, current_mob);
+}
+
 
 fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
-    // print all the lines be send to us
-    //println!("Parse room");
-    // for line in lines.iter() {
-    //     println!("Line: {}", line);
-    // }
 
-
-
-    let vnum: i32 = match lines.remove(0)[1..].parse() {
+    let vnum: u16 = match lines.remove(0)[1..].parse() {
         Ok(vnum) => vnum,
         Err(_) => {
             eprintln!("Error parsing vnum");
@@ -246,8 +369,6 @@ fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
     };
 
     let mut current_room = Room::new(vnum);
-    // println!("New Room: {}", vnum);
-
 
     let mut name = lines.remove(0);
     if name.ends_with('~') {
@@ -258,8 +379,6 @@ fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
         return;
     }
 
-    // println!("Room Name: {}", current_room.name);
-
     let (description, offset) = match parse_multi_line(&lines) {
         Ok(result) => result,
         Err(_) => {
@@ -268,7 +387,6 @@ fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
         }
     };
     current_room.description = description;
-    // println!("Room Description: {}", current_room.description);
 
     lines.drain(0..offset);
 
@@ -304,7 +422,7 @@ fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
 
     while !lines[0].starts_with('S') {
         if lines[0].starts_with('D') {
-            let door_dir: i32 = match lines.remove(0)[1..].parse() {
+            let door_dir: u8 = match lines.remove(0)[1..].parse() {
                 Ok(dir) => dir,
                 Err(_) => {
                     eprintln!("Error parsing door direction");
@@ -336,21 +454,21 @@ fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
                 eprintln!("Error parsing room - missing door info");
                 return;
             }
-            let door_locks: i32 = match door_info[0].parse() {
+            let door_locks: u16 = match door_info[0].parse() {
                 Ok(lock) => lock,
                 Err(_) => {
                     eprintln!("Error parsing door locks");
                     return;
                 }
             };
-            let door_key: i32 = match door_info[1].parse() {
+            let door_key: u16 = match door_info[1].parse() {
                 Ok(key) => key,
                 Err(_) => {
                     eprintln!("Error parsing door key");
                     return;
                 }
             };
-            let door_to_room: i32 = match door_info[2].parse() {
+            let door_to_room: u16 = match door_info[2].parse() {
                 Ok(room) => room,
                 Err(_) => {
                     eprintln!("Error parsing door to room");
@@ -367,8 +485,6 @@ fn parse_room(game: &mut Game, lines: &mut Vec<String>) {
                 to_room: door_to_room,
             };
             current_room.add_door(door);
-
-
 
         } else if lines[0].starts_with('E') {
             lines.remove(0);
@@ -454,8 +570,21 @@ fn parse_are_file(game: &mut Game, filename: &str) -> io::Result<()> {
                 state = State::None;
             }
             State::Mobiles => {
-                // Parse mobiles
-                state = State::None;
+                if line == "#0" {
+                    if !lines.is_empty() {
+                        parse_mob(game, &mut lines);  // Parse the last room
+                    }
+                    lines.clear();
+                    state = State::None;
+                } else if line.starts_with('#') {
+                    if !lines.is_empty() {
+                        parse_mob(game, &mut lines);  // Parse the current room
+                        lines.clear();  // Clear the vector for the next room
+                    }
+                    lines.push(line.to_string());  // Start collecting lines for the next room
+                } else {
+                    lines.push(line.to_string());  // Continue collecting lines for the current room
+                }
             }
             State::Objects => {
                 // Parse objects
@@ -463,7 +592,9 @@ fn parse_are_file(game: &mut Game, filename: &str) -> io::Result<()> {
             }
             State::Rooms => {
                 if line == "#0" {
-                    parse_room(game, &mut lines);  // Parse the last room
+                    if !lines.is_empty() {
+                        parse_room(game, &mut lines);  // Parse the last room
+                    }
                     lines.clear();
                     state = State::None;
                 } else if line.starts_with('#') {
@@ -496,38 +627,3 @@ fn parse_are_file(game: &mut Game, filename: &str) -> io::Result<()> {
 
     Ok(())
 }
-
-// fn main() {
-//     let mut game = Game {
-//         area_manager: AreaManager::new(),
-//         room_manager: RoomManager::new(),
-//     };
-
-//     parse_are_file(&mut game, "beach.are").expect("Failed to parse area file");
-
-
-//     // write me code that iterates over the rooms and prints out the room name and description
-//     for room in game.room_manager.get_all() {
-//         println!("Room: {} {}", room.vnum, room.name);
-//         println!("Description: {}", room.description);
-//         println!("Area Number: {}", room.area_number);
-//         println!("Room Flags: {}", room.room_flags);
-//         println!("Sector Type: {}", room.sector_type);
-//         println!("Doors: ");
-//         for door in &room.door_list {
-//             println!("Door: {} {}", door.door_number, door.description);
-//             println!("Keywords: {}", door.keywords);
-//             println!("Locks: {}", door.locks);
-//             println!("Key: {}", door.key);
-//             println!("To Room: {}", door.to_room);
-//         }
-//         println!("Extended Descriptions: ");
-//         for desc in &room.extended_descriptions_list {
-//             println!("Keywords: {}", desc.keywords);
-//             println!("Description: {}", desc.description);
-//         }
-//         println!("\n\n");
-//     }
-
-// }
-
